a950d871d27348fc4c6f21f4924c2f0b29cf76fc
Adam Wacławczyk <adam.waclawczyk@snow.dog>
2022-03-13 13:49:39 +0100

    Init commit

---
+++ src/CacheWarmer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Varmeusz\CacheWarmerSitemap;
+
+class CacheWarmer
+{
+    public function __construct()
+    {
+    }
+
+    public function loadSitemap()
+    {
+        return null;
+    }
+}
